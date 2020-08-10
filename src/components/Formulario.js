d315b03080eddbd54046e6caf40200fa9d0aabff
kevin <kevinalvapearson777@gmail.com>
2020-08-10 17:19:14 -0500

    creacion de formulario

---
+++ src/components/Formulario.js
@@ -0,0 +1,45 @@
+import React, {Fragment} from 'react';
+
+const Formulario = () => {
+    return ( 
+    <Fragment>
+        <h2>Crear cita</h2>
+        <form>
+            <label>Nombre mascota</label>
+            <input
+                type="text"
+                name="mascota"
+                className="u-full-width"
+                placeholder="Nombre mascota"
+            ></input>
+
+            <label>Nombre dueño</label>
+            <input
+                type="text"
+                name="propietario"
+                className="u-full-width"
+                placeholder="Nombre dueño de la mascota"
+            ></input>
+
+            <label>Fecha</label>
+            <input
+                type="date"
+                name="fecha"
+                className="u-full-width"
+            ></input>
+
+            <label>Hora</label>
+            <input
+                type="time"
+                name="hora"
+                className="u-full-width"
+            >
+            </input>
+
+
+        </form>
+    </Fragment>
+     );
+}
+ 
+export default Formulario;
