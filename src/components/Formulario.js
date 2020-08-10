ae182bce55f0757f2ae074883bc11b7f0f3185ab
kevin <kevinalvapearson777@gmail.com>
2020-08-10 17:48:34 -0500

    uso del useState para pasar informacion desde forumularios

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,21 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const Formulario = () => {
+
+    //Crear el state de citas
+    const [cita, actualizarCita] = useState({
+        mascota: '', 
+        propietario : '',
+        fecha : '',
+        hora : '', 
+        sintomas : ''
+    });
+
+    //Funcion que se ejecuta cada que un usuario escribe un post
+    const actualizarState = () =>{
+        console.log('escribiendo...');
+    }
+
     return ( 
     <Fragment>
         <h2>Crear cita</h2>
@@ -11,6 +26,7 @@ const Formulario = () => {
                 name="mascota"
                 className="u-full-width"
                 placeholder="Nombre mascota"
+                onChange={actualizarState}
             ></input>
 
             <label>Nombre dueño</label>
@@ -19,6 +35,8 @@ const Formulario = () => {
                 name="propietario"
                 className="u-full-width"
                 placeholder="Nombre dueño de la mascota"
+                onChange={actualizarState}
+
             ></input>
 
             <label>Fecha</label>
@@ -26,6 +44,8 @@ const Formulario = () => {
                 type="date"
                 name="fecha"
                 className="u-full-width"
+                onChange={actualizarState}
+
             ></input>
             
 
@@ -34,9 +54,23 @@ const Formulario = () => {
                 type="time"
                 name="hora"
                 className="u-full-width"
+                onChange={actualizarState}
+
             >
             </input>
 
+            <label>Síntomas</label>
+            <textarea
+                className="u-full-width"
+                name="sintomas"
+                onChange={actualizarState}
+
+            ></textarea>
+
+            <button
+                type="submit"
+                className="u-full-width button-primary"
+            >Agregar Cita</button>
 
         </form>
     </Fragment>
